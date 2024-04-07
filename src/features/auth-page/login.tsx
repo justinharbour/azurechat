b08222e45095c25cae9568d958d7cf6221f30cde
Justin Harbour <github@justinharbour.com>
2024-04-06 23:09:36 -0400

    tweak global settings

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -27,11 +27,11 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your Loyal Source-Issued Microsoft Account
+          Login in with your Loyal Source-Issued Employee Account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        <Button onClick={() => signIn("azure-ad")}> Sign in with your @loyalsource.com Account</Button>
         {props.isDevMode ? (
           <Button onClick={() => signIn("localdev")}>
             Basic Auth (DEV ONLY)
--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "LoyalGPT";
+export const AI_DESCRIPTION = "LoyalGPT is a friendly AI assistant designed to keep internal data private and provide access to advanced features on par with paid subscriptions to public models for free as part of your employment.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You're here to help employees with their everyday tasks. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
